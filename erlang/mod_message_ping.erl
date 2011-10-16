c1872bcc8f077d609be8a36b7ab8d8da95c00cf2
root <root@localhost.localdomain>
2011-10-16 13:03:31 -0400

    success mod_message_ping modules

--- erlang/mod_message_ping.erl
+++ erlang/mod_message_ping.erl
@@ -26,7 +26,6 @@
 
 -module(mod_message_ping).
 -author('romejiang@gmail.com').
-%% 简单状态机 和 默认的服务器接口
 -behavior(gen_mod).
 -behavior(gen_server).
 
@@ -40,7 +39,7 @@
 
 %% 定义一个数据集合，用来存储什么玩意
 -define(ETS_TABLE,history_message).
--define(IQ_TIMEOUT, 32). %% this is second
+-define(IQ_TIMEOUT, 33). %% this is second
 
 %% API
 -export([start_link/2, start_ping/2, stop_ping/2]).
@@ -53,7 +52,9 @@
          handle_info/2, code_change/3]).
 
 %% Hook callbacks
--export([iq_ping/3, user_online/3, user_offline/3, user_send/3]).
+-export([iq_ping/3, user_offline/3, user_send/3]).
+
+-export([time_to_second/1,kick_sessions/3,kick_this_session/4]).
 
 %% ets 数据结构
 -record(state, {host = "",
@@ -61,6 +62,7 @@
                 ping_interval = ?DEFAULT_PING_INTERVAL,
 				timeout_action = none}).
 
+-record(session, {sid, usr, us, priority, info}).
 %%====================================================================
 %% API
 %%====================================================================
@@ -114,8 +116,6 @@ init([Host, Opts]) ->
 	%% register callback funciton online, offline,user_send
     case SendPings of
         true ->
-            ejabberd_hooks:add(sm_register_connection_hook, Host,
-                               ?MODULE, user_online, 100),
             ejabberd_hooks:add(sm_remove_connection_hook, Host,
                                ?MODULE, user_offline, 100),
 			ejabberd_hooks:add(user_send_packet, Host,
@@ -132,8 +132,6 @@ init([Host, Opts]) ->
 terminate(_Reason, #state{host = Host}) ->
     ejabberd_hooks:delete(sm_remove_connection_hook, Host,
 			  ?MODULE, user_offline, 100),
-    ejabberd_hooks:delete(sm_register_connection_hook, Host,
-			  ?MODULE, user_online, 100),
     ejabberd_hooks:delete(user_send_packet, Host,
 			  ?MODULE, user_send, 100),
 	%%gen_iq_handler:remove_iq_handler(ejabberd_local, Host, ?NS_PING),
@@ -144,17 +142,19 @@ handle_call(stop, _From, State) ->
     {stop, normal, ok, State};
 handle_call(_Req, _From, State) ->
     {reply, {error, badarg}, State}.
-%% manager dict add del 
+%% send ping iq-message 
 handle_cast({start_ping, JID}, State) ->
     IQ = #iq{type = get,
 		sub_el = [{xmlelement, "ping", [{"xmlns", ?NS_PING}], []}]},
     Pid = self(),
-    F = fun(Response) ->
-        gen_server:cast(Pid, {iq_pong, JID, Response})
-    end,
     From = jlib:make_jid("", State#state.host, ""),
-    ?INFO_MSG("send ping message to ~p", [IQ]),
-    ejabberd_local:route_iq(From, JID, IQ, F),
+	#jid{user = User, server = Server} = JID,
+    lists:map(fun(Resource) ->
+    		?INFO_MSG("send ping message to ~p~p", [JID,Resource]),
+    		ejabberd_local:route_iq(From, jlib:jid_replace_resource(JID, Resource), IQ, 
+				fun(Response) ->gen_server:cast(Pid, {iq_pong, jlib:jid_replace_resource(JID, Resource) , Response}) end)
+		end,
+		get_resources(User, Server)),
     {noreply, State};
 handle_cast({stop_ping, _JID}, State) ->
     {noreply, State};
@@ -165,10 +165,13 @@ handle_cast({iq_pong, JID, timeout}, State) ->
     case State#state.timeout_action of
 	kill ->
 	    #jid{user = User, server = Server, resource = Resource} = JID,
-	    case ejabberd_sm:get_session_pid(User, Server, Resource) of
+		?INFO_MSG("kick kill ~p~p~p~p",[User, Server, Resource,ejabberd_sm:get_session_pid(User, Server, Resource)]),
+	    %%kick_sessions(User, Server, "timeout"),
+		%%resend_history_message(JID);
+		case ejabberd_sm:get_session_pid(User, Server, Resource) of
 		Pid when is_pid(Pid) ->
-		    ejabberd_c2s:stop(Pid),
-			resend_history_message(JID);
+		    ejabberd_c2s:stop(Pid);
+			%%resend_history_message(JID);
 		_ ->
 		    ok
 	    end;
@@ -177,10 +180,11 @@ handle_cast({iq_pong, JID, timeout}, State) ->
     end,
     {noreply, State};
 %% Received a reply
-handle_cast({iq_pong, JID, #iq{type = Type}	= IQquery}, State) ->
-	?INFO_MSG("IQ response info = ~p~p",[Type , IQquery] ),
+handle_cast({iq_pong, JID, #iq{type = Type}	= _IQquery}, State) ->
 	case Type of
-		"error" ->
+		%% FEATURE_NOT_IMPLEMENTED
+		error ->
+			?INFO_MSG("what is come here ??", []),
 			gen_server:cast(self(), {iq_pong, JID, timeout});
 		_ ->
 			clean_history_message(JID)
@@ -196,8 +200,8 @@ code_change(_OldVsn, State, _Extra) ->
 %%====================================================================
 %% Hook callbacks
 %%====================================================================
-iq_ping(_From, _To, #iq{type = Type, sub_el = SubEl} = IQ) ->
-	?INFO_MSG("iq ping callback ~p", [SubEl]),
+iq_ping(From, _To, #iq{type = Type, sub_el = SubEl} = IQ) ->
+	?INFO_MSG("iq ping callback ~p", [From]),
 	case {Type, SubEl} of
         {get, {xmlelement, "ping", _, _}} ->
             IQ#iq{type = result, sub_el = []};
@@ -205,35 +209,32 @@ iq_ping(_From, _To, #iq{type = Type, sub_el = SubEl} = IQ) ->
             IQ#iq{type = error, sub_el = [SubEl, ?ERR_FEATURE_NOT_IMPLEMENTED]}
     end.
 
-user_online(_SID, _JID, _Info) ->
-    %%start_ping(JID#jid.lserver, JID).
-	ok.
 
 user_offline(_SID, JID, _Info) ->
+	resend_history_message(JID),
     stop_ping(JID#jid.lserver, JID).
 
 user_send(From , JID, {xmlelement, "message", _, _} = Packet) ->
-	?INFO_MSG("user_send_packet send source = ~p",[Packet]),
+	?INFO_MSG("user_send_packet send source = ~p",[JID]),
     case xml:get_subtag_cdata(Packet, "body") of
 		"" ->
 			ok;
 		_Body ->
-			%% check JID online #########################################
 			case xml:get_tag_attr_s("type", Packet) of
 				"error" ->
 					?ERROR_MSG("Received error message~n~p -> ~p~n~p", [From, JID, Packet]);
 				_ ->
-					ets:insert(?ETS_TABLE ,{erlang:now(), JID, From, Packet}),
-					start_ping(JID#jid.lserver, JID)
+					start_ping(JID#jid.lserver,JID),
+					ets:insert(?ETS_TABLE ,{erlang:now(), JID, From, Packet})
 			end
     end;
-user_send(_JID, _From, _Packet) ->
-	?INFO_MSG("user_send_packet stop!!",[]),
+user_send(_From , _JID,_) ->
 	ok.
 %%====================================================================
 %% Internal functions
 %%====================================================================
-clean_history_message(JID) ->
+clean_history_message(JIDFull) ->
+	JID = jlib:jid_remove_resource(JIDFull),
 	Es = ets:select(?ETS_TABLE,[{{'_',JID,'_','_'},[],['$_']}]),
 	lists:foreach(fun({ID, _JID, _From, Packet}) ->
 			TimeDiff = time_diff(ID , erlang:now()),
@@ -243,18 +244,50 @@ clean_history_message(JID) ->
 		   		true -> ?INFO_MSG("~p ~p = ~p ~n", [time_to_second(ID) , TimeDiff , Packet])
 			end
 		end, Es).
-resend_history_message(JID) ->
+
+resend_history_message(JIDFull) ->
+	?INFO_MSG("resend JIDFull ~p ",[JIDFull]),
+	JID = jlib:jid_remove_resource(JIDFull), 
     Es = ets:select(?ETS_TABLE,[{{'_',JID,'_','_'},[],['$_']}]),
     lists:foreach(fun({ID, _JID, From, Packet}) ->
             TimeDiff = time_diff(ID , erlang:now()),
+			?INFO_MSG("time diff ~p ",[TimeDiff]),
             if
-                TimeDiff > ?IQ_TIMEOUT -> ets:delete(?ETS_TABLE, ID);
+                TimeDiff > ?IQ_TIMEOUT -> ok;
                 true -> 
+					?INFO_MSG("send offline message >>>>>>>>>>>>>>>>>>>>> ~p",[Packet]),
 					ejabberd_router:route(From, JID, Packet)
-            end
+            end,
+			ets:delete(?ETS_TABLE, ID)
         end, Es).
 time_diff({A1,A2,A3}, {B1,B2,B3}) -> 
     (B1 - A1) * 1000000 + (B2 - A2) + (B3 - A3) / 1000000.0 .
 time_to_second({T1,T2,T3}) ->
 	T1 * 1000000 + T2 + T3 / 1000000.0 .
 
+kick_sessions(User, Server, Reason) ->
+    lists:map(
+      fun(Resource) ->
+	      kick_this_session(User, Server, Resource, Reason)
+      end,
+      get_resources(User, Server)).
+
+get_resources(User, Server) ->
+    lists:map(
+      fun(Session) ->
+	      element(3, Session#session.usr)
+      end,
+      get_sessions(User, Server)).
+
+get_sessions(User, Server) ->
+    LUser = jlib:nodeprep(User),
+    LServer = jlib:nameprep(Server),
+    Sessions =  mnesia:dirty_index_read(session, {LUser, LServer}, #session.us),
+    true = is_list(Sessions),
+    Sessions.
+
+kick_this_session(User, Server, Resource, Reason) ->
+    ejabberd_router:route(
+      jlib:make_jid("", "", ""),
+      jlib:make_jid(User, Server, Resource),
+      {xmlelement, "broadcast", [], [{exit, Reason}]}).
