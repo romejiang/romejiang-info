49e6af589e1dd3a164fb61960aa502b541ae2633
romejiang <romejiang@gmail.com>
2011-12-06 16:06:12 +0800

    update sifang all

--- app/test.py
+++ app/test.py
@@ -1,12 +1,8 @@
-import winsound
-
-# Play Windows exit sound.
-#winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
-# Probably play Windows default sound, if any is registered (because
-# "*" probably isn't the registered name of any sound).
-#winsound.PlaySound("*", winsound.SND_ALIAS)
-
 print 'test'
-##winsound.Beep(frequency, duration)
-##winsound.Beep(32767,1000)
-winsound.Beep(1000,1000 * 3)
+while True:
+	msg = raw_input("Please input send message:")
+	if msg == "q":
+		print "exit program."
+		break
+	print "send to %s " %( msg )
+ 
